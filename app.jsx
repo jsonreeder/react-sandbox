1398a86fed4a56142bc1db972d4e7aab6f1c5d68
Jason Reeder <info@jasonreeder.com>
2017-03-05 11:26:39 -0800

    Initial commit

---
+++ app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Sandbox from './sandbox';
+
+document.addEventListener('DOMContentLoaded', function () {
+  ReactDOM.render(<Sandbox />, document.getElementById('main'));
+});
---
+++ sandbox.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Sandbox extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>React Sandbox</h1>
+        Scratch is scrutch. 
+      </div>
+    );
+  }
+}
+
+export default Sandbox;
